6b62a3ab01f9750a144cb4fb97bf165c0d190e8a
Jack Gruman <24gruman-jack@jonesboroschools.net>
2022-07-19 13:27:21 -0500

    Jumping and moving left and right. Ooh boy.

--- Assets/Scripts/EnvironmentScripts/enemyBehavior.cs
+++ Assets/Scripts/EnvironmentScripts/enemyBehavior.cs
@@ -7,12 +7,16 @@ public class enemyBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Rigidbody m_Rigidbody;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < 5; i++) ;
+        {
+            //m_Rigidbody.AddForce(transform.left * 5);
+        }
     }
 }
