4b543958a1a866fd7a0422cc7b7ef3c0e9503aad
Jack Gruman <24gruman-jack@jonesboroschools.net>
2022-07-19 13:04:40 -0500

    movement script and red guys enemies

---
+++ Assets/Scripts/PlayerScripts/playerBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehavior : MonoBehaviour
+{
+
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    { }
+    public float speed = 7;
+
+    public float jump = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y); //Horizontal movement.
+
+    }
+    void OnTriggerStay2D(Collider2D collider) //Makes character leave the ground.
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y + 1);
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider) //Executes the jump.
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y + jump);
+        }
+    }
+
+}
