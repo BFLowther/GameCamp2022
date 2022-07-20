9166ee84da5c7c80d2e645f3e2ab264263280962
Ross Hart <Archerbro72@gmail.com>
2022-07-20 16:33:52 -0500

    Added Health pickup

---
+++ Assets/Scripts/PlayerScripts/HealthPickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public int potion = 1;
+    public GameObject Player;
+    public playerBehavior pb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pb = Player.GetComponent<playerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("potion"))
+        {
+            pb.health += potion;
+            other.transform.gameObject.SetActive(false);
+            other.gameObject.tag = "Untagged";
+            Destroy(other.gameObject);
+        }
+    }
+}
