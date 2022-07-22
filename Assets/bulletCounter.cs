4f7f34881bf3c18e13b9a3b096886f819f5f7357
Jack Gruman <24gruman-jack@jonesboroschools.net>
2022-07-22 14:41:26 -0500

    better looking bullet counter!

--- Assets/bulletCounter.cs
+++ Assets/bulletCounter.cs
@@ -12,6 +12,6 @@ public class bulletCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = "" + player.RangedWeapons.bulletPack.bullets;
+        text.text = "Bullets Left:" + player.RangedWeapons.bulletPack.bullets;
     }
 }
