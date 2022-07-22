1001357c6c0422d3ec86d066eeacfe3e1257ceb4
Michael Rhyan <5miles5085@gmail.com>
2022-07-22 18:39:52 -0500

    End Scene Animation

--- Assets/Scripts/UIScripts/SceneLoader.cs
+++ Assets/Scripts/UIScripts/SceneLoader.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Animator transitionAnim;
     Scene scene;
     public void sceneChange()
     {
         //Change to next scene
         scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+    public void fadeAnim()
+    {
+        transitionAnim.SetTrigger("endScene");
     }
     public void backToStartChange()
     {
