85b64a1ddee4227c698404621ebdb53fc1c86d75
Jack Gruman <24gruman-jack@jonesboroschools.net>
2022-07-22 12:57:51 -0500

    Working health bar and music!

--- Assets/healthBar.cs
+++ Assets/healthBar.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 public class healthBar : MonoBehaviour
 {
+    public int currentHealth;
     public Slider slider;
 
    public void SetHealth(int health)
     {
-        slider.value = health;
+        currentHealth = health;
+        slider.value = currentHealth;
     }
 
-    public void SetMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
+   public void SetMaxHealth(int health)
+   {
+       slider.maxValue = health;
+       slider.value = health;
+   }
 
-    public Transform target;
 
-    public float smoothSpeed = 0.125f;
 
-    public Vector3 offset;
+   public Transform target;
 
-    void LateUpdate() //happens after update to not compete with movement
-    {
-        transform.position = target.position + offset;
+   public float smoothSpeed = 0.01f;
 
-    }
+   public Vector3 offset2;
+
+   void LateUpdate() //happens after update to not compete with movement
+   {
+        //transform.position = target.position + offset2;
+
+   }
     
 
 }
